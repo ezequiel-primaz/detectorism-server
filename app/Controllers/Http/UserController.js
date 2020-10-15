34e622d929b822533ec593299bc8287b95a7cdcc
Ezequiel Primaz <ezequiel@primaz.dev>
2020-10-15 20:54:53 -0300

    Fixing start script.

--- app/Controllers/Http/UserController.js
+++ app/Controllers/Http/UserController.js
@@ -11,21 +11,21 @@ class UserController {
         return user
     }
 
-    async show ({ params }) {
+    async show({ params }) {
         const user = await User.findOrFail(params.id)
-    
+
         const treasures = await user
-          .treasures()
-          .with('user', (builder) => {
-            builder.select('id', 'username', 'email')
-          })
-          .with('images')
-          .fetch()
-    
+            .treasures()
+            .with('user', (builder) => {
+                builder.select('id', 'username', 'email')
+            })
+            .with('images')
+            .fetch()
+
         user.treasures = treasures
-    
+
         return user
-      }
+    }
 }
 
 module.exports = UserController
